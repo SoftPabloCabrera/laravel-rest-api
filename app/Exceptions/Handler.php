224c5d9646e317aa3941a2a0340cf6415b2c62fa
unknown <dev.cabrera.pablo@gmail.com>
2022-01-13 00:43:49 -0600

    Update Handler.php file to exclude all unnecesary json, also RouteServiceProvider.php to resolve routes uncommenting protected namespace

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -35,7 +37,15 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if($exception instanceof ModelNotFoundException){
+            return response()->json(0,404);
+        }
+        return parent::render($request, $exception);
+
     }
 }
